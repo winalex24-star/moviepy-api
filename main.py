75705391d8cd370334d5fe24f48ef19083fbe47a
winalex24-star <winalex24@gmail.com>
2026-09-24 22:31:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -65,12 +65,14 @@ def create_video(req: VideoRequest):
 
         print(f"Procesando: {archivo}")
 
-        imagen = (
-            ImageClip(archivo)
-            .resized(height=480)
-            .with_duration(1)
-            .with_position("center")
-        )
+       imagen = (
+    ImageClip(archivo)
+    .resized(height=480)
+    .with_duration(2)
+    .with_position("center")
+    .resized(lambda t: 1 + 0.08 * t)
+)
+`
 
         fondo = (
             ColorClip(
