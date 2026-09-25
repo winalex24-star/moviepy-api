677cda7b02873e4f70f36affd7fb2111f9eb49b2
winalex24-star <winalex24@gmail.com>
2026-09-24 19:35:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class VideoRequest(BaseModel):
+    titulo: str
+    imagen1: str
+    imagen2: str
+    imagen3: str
+    imagen4: str
+    imagen5: str
 
 @app.get("/")
 def health():
     return {
         "status": "ok",
-        "service": "moviepy-api",
-        "version": "1.0"
+        "service": "moviepy-api"
     }
 
 @app.post("/video")
-def create_video():
+def create_video(req: VideoRequest):
     return {
-        "message": "Video endpoint funcionando"
+        "titulo": req.titulo,
+        "imagenes": [
+            req.imagen1,
+            req.imagen2,
+            req.imagen3,
+            req.imagen4,
+            req.imagen5
+        ]
     }
