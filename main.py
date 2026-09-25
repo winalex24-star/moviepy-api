7dfc4e946a2b8c9f9276f94de73351c56c87e91a
winalex24-star <winalex24@gmail.com>
2026-09-24 19:58:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
+from moviepy import ImageClip, concatenate_videoclips
+import requests
+import uuid
 
 app = FastAPI()
 
@@ -11,23 +15,56 @@ class VideoRequest(BaseModel):
     imagen4: str
     imagen5: str
 
+def descargar_imagen(url, archivo):
+    r = requests.get(url)
+    with open(archivo, "wb") as f:
+        f.write(r.content)
+
 @app.get("/")
 def health():
-    return {
-        "status": "ok"
-    }
+    return {"status": "ok"}
 
 @app.post("/video")
 def create_video(req: VideoRequest):
 
-    return {
-        "status": "ok",
-        "titulo": req.titulo,
-        "imagenes": [
-            req.imagen1,
-            req.imagen2,
-            req.imagen3,
-            req.imagen4,
-            req.imagen5
-        ]
-    }
+    imagenes = [
+        req.imagen1,
+        req.imagen2,
+        req.imagen3,
+        req.imagen4,
+        req.imagen5,
+    ]
+
+    archivos = []
+
+    for i, url in enumerate(imagenes):
+        nombre = f"img_{i}.jpg"
+        descargar_imagen(url, nombre)
+        archivos.append(nombre)
+
+    clips = []
+
+    for archivo in archivos:
+        clip = (
+            ImageClip(archivo)
+            .resized(height=1920)
+            .with_duration(3)
+        )
+
+        clips.append(clip)
+
+    video = concatenate_videoclips(clips)
+
+    nombre_video = f"{uuid.uuid4()}.mp4"
+
+    video.write_videofile(
+        nombre_video,
+        fps=24,
+        codec="libx264"
+    )
+
+    return FileResponse(
+        nombre_video,
+        media_type="video/mp4",
+        filename="video.mp4"
+    )
